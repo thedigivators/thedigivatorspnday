caf72a59b7773da3f3146887fd649a631eca0bc1
thedigivators <thedigivatorss@gmail.com>
2025-04-15 23:57:25 +0700

    Update SAMSUNG INNOVATION CAMPUS.py

--- pages/SAMSUNG INNOVATION CAMPUS.py
+++ pages/SAMSUNG INNOVATION CAMPUS.py
@@ -1,22 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from datetime import time
-
-pages = {
-    "Our Product": [
-        st.Page("Dashboard-SIPANDAIHSC191.py", title="SIPANDAI"),
-        st.Page("pages/halamangenai.py", title="SIPANDAI SARAN"),
-        st.Page("pages/halamanlaporan.py", title="SIPANDAI LAPORAN")
-    ],
-    "About Us": [
-        st.Page("pages/SAMSUNG INNOVATION CAMPUS.py", title="Samsung Innovation Campus"),
-        st.Page("pages/thedigivators.py", title="The Digivators"),
-    ],
-}
-
-pg = st.navigation(pages)
-pg.run() 
+from datetime import time 
 
 st.title("🎓 SAMSUNG INNOVATION CAMPUS")
 st.write("From HSC191-The Digivators-SMAN 1 Rejang Lebong")
