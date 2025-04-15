91411af9d8eabde4ba39ecc3117ddfe746a1b451
thedigivators <thedigivatorss@gmail.com>
2025-04-15 23:54:23 +0700

    Create SiPages.py

---
+++ pages/SiPages.py
@@ -0,0 +1,16 @@
+import streamlit as st 
+
+pages = {
+    "Our Product": [
+        st.Page("Dashboard-SIPANDAIHSC191.py", title="SIPANDAI"),
+        st.Page("pages/halamangenai.py", title="SIPANDAI SARAN"),
+        st.Page("pages/halamanlaporan.py", title="SIPANDAI LAPORAN")
+    ],
+    "About Us": [
+        st.Page("pages/SAMSUNG INNOVATION CAMPUS.py", title="Samsung Innovation Campus"),
+        st.Page("pages/thedigivators.py", title="The Digivators"),
+    ],
+}
+
+pg = st.navigation(pages)
+pg.run() 
