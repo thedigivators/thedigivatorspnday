74337fb73b2d5525e3fcee7565f56804d61842f4
thedigivators <thedigivatorss@gmail.com>
2025-04-17 19:43:04 +0700

    Update deteksi.py

--- deteksi.py
+++ deteksi.py
@@ -32,7 +32,7 @@ if st.button("Update"):
     prompt = f"""
     berdasarkan data, total deteksi handphone sebanyak = {detection_value} kali, pada ujian matematika dengan durasi 1 jam, menggunakan metode deteksi handphone dengan model YOLO
     jumlah siswa dalam 1 ruangan 25 orang
-    berikan pendapat anda tentang ruangan ini berdasarkan banyaknya total deteksi handphone.
+    berikan pendapat singkat anda tentang ruangan ini berdasarkan banyaknya total deteksi handphone.
     """
     response = model.generate_content(prompt)
 
