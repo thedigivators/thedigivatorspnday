7983445917119eed70f2cd0ca5db9a6cdb3f9f82
thedigivators <thedigivatorss@gmail.com>
2025-04-18 11:45:55 +0700

    Update deteksi.py

--- deteksi.py
+++ deteksi.py
@@ -14,7 +14,7 @@ if "llm" not in st.session_state:
 
 
 
-st.title("‼️SIPANDAI Deteksi aksi mencontek dengan AI❗")
+st.title("SIPANDAI Deteksi Contek dengan AI")
 st.write("The Digivators-HSC191-SMAN1RL, SIC6. We born to gain & get not to be bad(🫸cheat)")
 
 col1, col2 = st.columns(2)
