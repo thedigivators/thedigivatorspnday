93e9371f8c7644fa40f4520add76c7110b926e71
thedigivators <thedigivatorss@gmail.com>
2025-05-03 16:23:57 +0700

    Update utama.py

--- utama.py
+++ utama.py
@@ -6,7 +6,6 @@ pages = {
         st.Page("halamangenai.py", title="Saran"),
         st.Page("halamanlaporan.py", title="Laporan"),
         st.Page("grafik.py", title="Riwayat"),
-        st.Page("grafik2.py", title="Riwayat real time"),
     ],
     "About Us": [
         st.Page("aboutsipandai.py", title="Tentang SIPANDAI"),
